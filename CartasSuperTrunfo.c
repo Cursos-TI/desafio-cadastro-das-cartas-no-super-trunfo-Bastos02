bc1a901aaeccb472b49cf295b87b6c9ba99a7ff1
Bastos <guilhermeaugusto0606060@gmail.com>
2025-07-31 21:42:43 +0000

    segunda carta apresentada

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    //Variaveis da primeira carta 01
+//Variaveis da primeira carta 01
     char Estado[50];
     char Cidade[50];
     int Populacao;
@@ -12,7 +12,7 @@ int main() {
     char codigo[50];
 
 //Titulo do programa
-    printf("Cadastro das Cartas\n   super trunfo\n");
+    printf("\nCadastro das Cartas\n   super trunfo\n");
 
 //Informações da primeira carta 
     printf("\n   Carta 01\n");
@@ -49,6 +49,52 @@ int main() {
     printf("PIB: R$ %.2f bilhões\n", PIB);
     printf("PONTOS TURÍSTICOS: %d\n", PontosTuristicos);
 
+//Variaveis da segunda carta 02
+    char Estado2[50];
+    char Cidade2[50];
+    int Populacao2;
+    double Area2;
+    float PIB2;
+    int PontosTuristicos2;
+    char codigo2[50];
+
+//Titulo do programa
+    printf("\n Cadastro das Cartas\n   super trunfo\n");
+
+//Informações da primeira carta 
+    printf("\n   Carta 02\n");
+
+//Informações do primeiro Estado
+    printf("Nome do Estado: ");
+    scanf("%s", Estado2);
+    printf("Código da Carta: ");
+    scanf("%s", codigo2);
+
+//informações da primeira cidade
+    printf("Nome da Cidade: ");
+    scanf("%s", Cidade2);
+    
+    printf("População: ");
+    scanf("%d", &Populacao2);
+
+    printf("Área: ");
+    scanf("%lf", &Area2);
+
+    printf("PIB: ");
+    scanf("%f", &PIB2);
+
+    printf("Número de Pontos turísticos: ");
+    scanf("%d", &PontosTuristicos2);
+
+//carta apresentada
+    printf("\n   Carta Apresentada\n");
+    printf("CÓDIGO: %s\n", codigo2);
+    printf("ESTADO: %s\n", Estado2);
+    printf("CIDADE: %s\n", Cidade2);
+    printf("POPULAÇÃO: %d\n", Populacao2);
+    printf("ÁREA: %.2lf km²\n", Area2);
+    printf("PIB: R$ %.2f bilhões\n", PIB2);
+    printf("PONTOS TURÍSTICOS: %d\n", PontosTuristicos2);
 
 
 return 0;
