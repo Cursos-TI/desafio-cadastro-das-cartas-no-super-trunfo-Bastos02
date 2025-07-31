2e94ab46d25cbc26beea7b4c531ed4a65ec7fa86
Bastos <guilhermeaugusto0606060@gmail.com>
2025-07-31 17:46:22 +0000

    Adicionando informaçoes da primeira cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 int main() {
 
     char Estado[50];
-
+    char Cidade[50];
 
 //Titulo do programa
     printf("Cadastro das Cartas\n   super trunfo\n");
@@ -12,11 +12,12 @@ int main() {
     printf("\n   Carta 01\n");
 
 //Informações do primeiro Estado
-    printf("Nome do Estado:\n");
+    printf("Nome do Estado: ");
     scanf("%s", Estado);
 
 //informações da primeira cidade
-    printf("")
+    printf("Nome da Cidade: ");
+    scanf("%s", Cidade);
 
 
 
