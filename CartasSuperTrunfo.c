f0c5a3f3117a7721b8eb2ca0345050cc75502534
Bastos <guilhermeaugusto0606060@gmail.com>
2025-07-31 17:43:17 +0000

    Adicionando documentação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,15 +5,18 @@ int main() {
     char Estado[50];
 
 
-
-
-
+//Titulo do programa
     printf("Cadastro das Cartas\n   super trunfo\n");
 
+//Informações da primeira carta 
     printf("\n   Carta 01\n");
+
+//Informações do primeiro Estado
     printf("Nome do Estado:\n");
     scanf("%s", Estado);
 
+//informações da primeira cidade
+    printf("")
 
 
 
